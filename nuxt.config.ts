33a909afb64bec0b6f3718164746393adcb47a2f
Romwel Feliciano <romwel@spectorandco.com>
2025-06-17 03:06:29 +0800

    i18n: default locale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/image"],
-  css: ["~/assets/css/main.css"],
+  modules: ['@nuxt/image', '@nuxtjs/i18n'],
+  css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  i18n: {
+    strategy: 'prefix', 
+    defaultLocale: 'us',
+    detectBrowserLanguage: false, 
+    locales: [
+      {
+        code: 'us',
+        iso: 'en-US',
+        name: 'United States',
+        file: 'us.json',
+      },
+      {
+        code: 'en',
+        iso: 'en-GB',
+        name: 'English',
+        file: 'en.json',
+      },
+      {
+        code: 'fr',
+        iso: 'fr-FR',
+        name: 'Français',
+        file: 'fr.json',
+      },
+    ],
+    lazy: true, 
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
-<script setup></script>
+<script setup>
+const { locales, setLocale } = useI18n();
+const route = useRoute();
+</script>
 
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="flex items-center gap-2">
+    <button v-for="locale in locales" @click="setLocale(locale.code)" class="rounded bg-yellow-500 px-4 py-2 text-black">
+      {{ locale.name }}
+    </button>
   </div>
+  <h1>{{ $t('welcome') }}</h1>
+  <p>{{ $t('description') }}</p>
+  <p>{{ route.path }}</p>
 </template>
 
 <style scoped></style>
